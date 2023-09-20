e29ae8549be605f8927f1fff90d54ebdeed13ec9
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-09-20 13:29:14 +0800

    m

--- src/app/modules/faculty/faculty.service.ts
+++ src/app/modules/faculty/faculty.service.ts
@@ -387,6 +387,39 @@ const createFacultyFromEvent = async (
   const data = await insertIntoDB(faculty as Faculty);
   console.log('RES: ', data);
 };
+const updateFacultyFromEvent = async (e: any): Promise<void> => {
+  const isExist = await prisma.faculty.findFirst({
+    where: {
+      facultyId: e.id,
+    },
+  });
+  if (!isExist) {
+    createFacultyFromEvent(e);
+  } else {
+    const facultyData: Partial<Faculty> = {
+      facultyId: e.id,
+      firstName: e.name.firstName,
+      lastName: e.name.lastName,
+      middleName: e.name.middleName,
+      profileImage: e.profileImage,
+      email: e.email,
+      contactNo: e.contactNo,
+      gender: e.gender,
+      bloodGroup: e.bloodGroup,
+      designation: e.designation,
+      academicDepartmentId: e.academicDepartment.syncId,
+      academicFacultyId: e.academicFaculty.syncId,
+    };
+
+    const res = await prisma.faculty.updateMany({
+      where: {
+        facultyId: e.id,
+      },
+      data: facultyData,
+    });
+    console.log(res);
+  }
+};
 export const FacultyService = {
   insertIntoDB,
   getAllFromDB,
@@ -398,4 +431,5 @@ export const FacultyService = {
   myCourses,
   getMyCourseStudents,
   createFacultyFromEvent,
+  updateFacultyFromEvent,
 };
